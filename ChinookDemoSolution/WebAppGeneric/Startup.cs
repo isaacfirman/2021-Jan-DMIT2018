b31b7a20e0fb56c9f69311382f22051da32e184c
unknown <isaac.firman@gmail.com>
2021-01-07 16:36:06 -0700

    First update

---
+++ ChinookDemoSolution/WebAppGeneric/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppGeneric.Startup))]
+namespace WebAppGeneric
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
